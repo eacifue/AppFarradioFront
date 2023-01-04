1d67b916541da3f6f01e0f89533aeed08c60d609
Eric Cifuentes <eaifue@gmail.com>
2023-01-04 12:16:31 -0500

    changes in search producr

--- farradio-app/src/pages/inventario.js
+++ farradio-app/src/pages/inventario.js
@@ -1,34 +1,39 @@
-import React, {useState}  from 'react';
-import {Container, Row, Col, Form, Button, InputGroup} from 'react-bootstrap';
-import TableComponet from '../componets/Table'
-export default function Inventario(props){
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Form, Button, InputGroup } from 'react-bootstrap';
+import TableComponet from '../componets/Table';
 
-    const [searchText, setSearchText] = useState('')
-    const [products, setProducts] = useState([])
+export default function Inventario(props) {
+  const [searchText, setSearchText] = useState('');
+  const [products, setProducts] = useState([]);
 
-const handelEnterKeyPressed = (e) =>{
-    const text = e.target.value
-    setSearchText(text)
-    if(e.key==="Enter"){       
-        getProducts(searchText)
+  const search = async () => {
+    try {
+      const res = await fetch(`http://localhost:9000/api/productos/${searchText}`);
+      const data = await res.json();
+      setProducts(data);
+    } catch (error) {
+      console.error(error);
     }
-}
-const getProducts = () =>{
-    fetch('http://localhost:9000/api/productos/'+ searchText)
-    .then(res => res.json())
-    .then(res => setProducts(res))
-} 
+  };
 
-  /*  const [products, setProducts] = useState([])
-    useEffect(() =>{
-        const getProducts = () =>{
-            fetch('http://localhost:9000/api/productos/')
-            .then(res => res.json())
-            .then(res => setProducts(res))
-        }
-        getProducts()
-    }, [])*/
+  useEffect(() => {
+    if (searchText) {
+      search();
+    }
+  }, [searchText]);
 
+  const handleSearchClick = () => {
+    search();
+  };
+
+  const handleEnterKeyPressed = (e) => {
+    const text = e.target.value;
+    setSearchText(text);
+
+    if (e.key === 'Enter') {
+      search();
+    }
+  };
     return (
     <Container fluid="md">
         <Row>
@@ -37,7 +42,7 @@ const getProducts = () =>{
                     <Form.Group className="sb-2" controlId="txtSearch">
                         <Form.Label><b>Producto a Buscar</b></Form.Label>
                         <InputGroup className="mb-3">
-                            <Form.Control type="text"  onKeyPress={handelEnterKeyPressed} placeholder="Search" />
+                            <Form.Control type="text"  onKeyPress={handleEnterKeyPressed} placeholder="Search" />
                             <Button variant="outline-primary">Buscar</Button>{}
                         </InputGroup>
                     </Form.Group>
